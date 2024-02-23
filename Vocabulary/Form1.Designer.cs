b02c7d90f42424d2173dae92edf5ffde498b66ac
PaFleischer <pascal.fleischer@tsbw.cloud>
2024-02-23 07:59:32 +0100

    initial release

---
+++ Vocabulary/Form1.Designer.cs
@@ -0,0 +1,375 @@
+﻿namespace Vocabulary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Vocablbl1 = new System.Windows.Forms.Label();
+            this.Vocablbl2 = new System.Windows.Forms.Label();
+            this.Vocablbl3 = new System.Windows.Forms.Label();
+            this.Vocablbl4 = new System.Windows.Forms.Label();
+            this.Vocablbl5 = new System.Windows.Forms.Label();
+            this.Vocablbl6 = new System.Windows.Forms.Label();
+            this.Vocablbl7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.entry1 = new System.Windows.Forms.TextBox();
+            this.entry2 = new System.Windows.Forms.TextBox();
+            this.refrshbtn = new System.Windows.Forms.Button();
+            this.Englbl1 = new System.Windows.Forms.TextBox();
+            this.Englbl2 = new System.Windows.Forms.TextBox();
+            this.Englbl3 = new System.Windows.Forms.TextBox();
+            this.Englbl4 = new System.Windows.Forms.TextBox();
+            this.Englbl5 = new System.Windows.Forms.TextBox();
+            this.Englbl6 = new System.Windows.Forms.TextBox();
+            this.Englbl7 = new System.Windows.Forms.TextBox();
+            this.hideAnswer1 = new System.Windows.Forms.Button();
+            this.hideAnswer2 = new System.Windows.Forms.Button();
+            this.hideAnswer3 = new System.Windows.Forms.Button();
+            this.hideAnswer4 = new System.Windows.Forms.Button();
+            this.hideAnswer5 = new System.Windows.Forms.Button();
+            this.hideAnswer6 = new System.Windows.Forms.Button();
+            this.hideAnswer7 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Vocablbl1
+            // 
+            this.Vocablbl1.AutoSize = true;
+            this.Vocablbl1.Location = new System.Drawing.Point(42, 81);
+            this.Vocablbl1.Name = "Vocablbl1";
+            this.Vocablbl1.Size = new System.Drawing.Size(0, 13);
+            this.Vocablbl1.TabIndex = 0;
+            // 
+            // Vocablbl2
+            // 
+            this.Vocablbl2.AutoSize = true;
+            this.Vocablbl2.Location = new System.Drawing.Point(42, 131);
+            this.Vocablbl2.Name = "Vocablbl2";
+            this.Vocablbl2.Size = new System.Drawing.Size(0, 13);
+            this.Vocablbl2.TabIndex = 2;
+            // 
+            // Vocablbl3
+            // 
+            this.Vocablbl3.AutoSize = true;
+            this.Vocablbl3.Location = new System.Drawing.Point(42, 181);
+            this.Vocablbl3.Name = "Vocablbl3";
+            this.Vocablbl3.Size = new System.Drawing.Size(0, 13);
+            this.Vocablbl3.TabIndex = 4;
+            // 
+            // Vocablbl4
+            // 
+            this.Vocablbl4.AutoSize = true;
+            this.Vocablbl4.Location = new System.Drawing.Point(42, 231);
+            this.Vocablbl4.Name = "Vocablbl4";
+            this.Vocablbl4.Size = new System.Drawing.Size(0, 13);
+            this.Vocablbl4.TabIndex = 6;
+            // 
+            // Vocablbl5
+            // 
+            this.Vocablbl5.AutoSize = true;
+            this.Vocablbl5.Location = new System.Drawing.Point(42, 281);
+            this.Vocablbl5.Name = "Vocablbl5";
+            this.Vocablbl5.Size = new System.Drawing.Size(0, 13);
+            this.Vocablbl5.TabIndex = 8;
+            // 
+            // Vocablbl6
+            // 
+            this.Vocablbl6.AutoSize = true;
+            this.Vocablbl6.Location = new System.Drawing.Point(42, 331);
+            this.Vocablbl6.Name = "Vocablbl6";
+            this.Vocablbl6.Size = new System.Drawing.Size(0, 13);
+            this.Vocablbl6.TabIndex = 10;
+            // 
+            // Vocablbl7
+            // 
+            this.Vocablbl7.AutoSize = true;
+            this.Vocablbl7.Location = new System.Drawing.Point(42, 381);
+            this.Vocablbl7.Name = "Vocablbl7";
+            this.Vocablbl7.Size = new System.Drawing.Size(0, 13);
+            this.Vocablbl7.TabIndex = 12;
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(385, 491);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Save Entry";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // entry1
+            // 
+            this.entry1.Location = new System.Drawing.Point(45, 497);
+            this.entry1.Name = "entry1";
+            this.entry1.Size = new System.Drawing.Size(100, 20);
+            this.entry1.TabIndex = 15;
+            // 
+            // entry2
+            // 
+            this.entry2.Location = new System.Drawing.Point(264, 494);
+            this.entry2.Name = "entry2";
+            this.entry2.Size = new System.Drawing.Size(100, 20);
+            this.entry2.TabIndex = 16;
+            // 
+            // refrshbtn
+            // 
+            this.refrshbtn.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.refrshbtn.Location = new System.Drawing.Point(45, 432);
+            this.refrshbtn.Name = "refrshbtn";
+            this.refrshbtn.Size = new System.Drawing.Size(75, 23);
+            this.refrshbtn.TabIndex = 17;
+            this.refrshbtn.Text = "Refresh";
+            this.refrshbtn.UseVisualStyleBackColor = true;
+            this.refrshbtn.Click += new System.EventHandler(this.refrshbtn_Click);
+            // 
+            // Englbl1
+            // 
+            this.Englbl1.Enabled = false;
+            this.Englbl1.Location = new System.Drawing.Point(264, 74);
+            this.Englbl1.Name = "Englbl1";
+            this.Englbl1.Size = new System.Drawing.Size(100, 20);
+            this.Englbl1.TabIndex = 18;
+            // 
+            // Englbl2
+            // 
+            this.Englbl2.Enabled = false;
+            this.Englbl2.Location = new System.Drawing.Point(264, 124);
+            this.Englbl2.Name = "Englbl2";
+            this.Englbl2.Size = new System.Drawing.Size(100, 20);
+            this.Englbl2.TabIndex = 19;
+            // 
+            // Englbl3
+            // 
+            this.Englbl3.Enabled = false;
+            this.Englbl3.Location = new System.Drawing.Point(264, 173);
+            this.Englbl3.Name = "Englbl3";
+            this.Englbl3.Size = new System.Drawing.Size(100, 20);
+            this.Englbl3.TabIndex = 20;
+            // 
+            // Englbl4
+            // 
+            this.Englbl4.Enabled = false;
+            this.Englbl4.Location = new System.Drawing.Point(264, 223);
+            this.Englbl4.Name = "Englbl4";
+            this.Englbl4.Size = new System.Drawing.Size(100, 20);
+            this.Englbl4.TabIndex = 21;
+            // 
+            // Englbl5
+            // 
+            this.Englbl5.Enabled = false;
+            this.Englbl5.Location = new System.Drawing.Point(264, 273);
+            this.Englbl5.Name = "Englbl5";
+            this.Englbl5.Size = new System.Drawing.Size(100, 20);
+            this.Englbl5.TabIndex = 22;
+            // 
+            // Englbl6
+            // 
+            this.Englbl6.Enabled = false;
+            this.Englbl6.Location = new System.Drawing.Point(264, 323);
+            this.Englbl6.Name = "Englbl6";
+            this.Englbl6.Size = new System.Drawing.Size(100, 20);
+            this.Englbl6.TabIndex = 23;
+            // 
+            // Englbl7
+            // 
+            this.Englbl7.Enabled = false;
+            this.Englbl7.Location = new System.Drawing.Point(264, 373);
+            this.Englbl7.Name = "Englbl7";
+            this.Englbl7.Size = new System.Drawing.Size(100, 20);
+            this.Englbl7.TabIndex = 24;
+            // 
+            // hideAnswer1
+            // 
+            this.hideAnswer1.BackColor = System.Drawing.Color.Black;
+            this.hideAnswer1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.hideAnswer1.Location = new System.Drawing.Point(264, 71);
+            this.hideAnswer1.Name = "hideAnswer1";
+            this.hideAnswer1.Size = new System.Drawing.Size(100, 23);
+            this.hideAnswer1.TabIndex = 25;
+            this.hideAnswer1.UseVisualStyleBackColor = false;
+            this.hideAnswer1.Click += new System.EventHandler(this.hideAnswer_Click);
+            // 
+            // hideAnswer2
+            // 
+            this.hideAnswer2.BackColor = System.Drawing.Color.Black;
+            this.hideAnswer2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.hideAnswer2.Location = new System.Drawing.Point(264, 121);
+            this.hideAnswer2.Name = "hideAnswer2";
+            this.hideAnswer2.Size = new System.Drawing.Size(100, 23);
+            this.hideAnswer2.TabIndex = 26;
+            this.hideAnswer2.UseVisualStyleBackColor = false;
+            this.hideAnswer2.Click += new System.EventHandler(this.hideAnswer_Click);
+            // 
+            // hideAnswer3
+            // 
+            this.hideAnswer3.BackColor = System.Drawing.Color.Black;
+            this.hideAnswer3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.hideAnswer3.Location = new System.Drawing.Point(264, 170);
+            this.hideAnswer3.Name = "hideAnswer3";
+            this.hideAnswer3.Size = new System.Drawing.Size(100, 23);
+            this.hideAnswer3.TabIndex = 27;
+            this.hideAnswer3.UseVisualStyleBackColor = false;
+            this.hideAnswer3.Click += new System.EventHandler(this.hideAnswer_Click);
+            // 
+            // hideAnswer4
+            // 
+            this.hideAnswer4.BackColor = System.Drawing.Color.Black;
+            this.hideAnswer4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.hideAnswer4.Location = new System.Drawing.Point(264, 220);
+            this.hideAnswer4.Name = "hideAnswer4";
+            this.hideAnswer4.Size = new System.Drawing.Size(100, 23);
+            this.hideAnswer4.TabIndex = 28;
+            this.hideAnswer4.UseVisualStyleBackColor = false;
+            this.hideAnswer4.Click += new System.EventHandler(this.hideAnswer_Click);
+            // 
+            // hideAnswer5
+            // 
+            this.hideAnswer5.BackColor = System.Drawing.Color.Black;
+            this.hideAnswer5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.hideAnswer5.Location = new System.Drawing.Point(264, 270);
+            this.hideAnswer5.Name = "hideAnswer5";
+            this.hideAnswer5.Size = new System.Drawing.Size(100, 23);
+            this.hideAnswer5.TabIndex = 29;
+            this.hideAnswer5.UseVisualStyleBackColor = false;
+            this.hideAnswer5.Click += new System.EventHandler(this.hideAnswer_Click);
+            // 
+            // hideAnswer6
+            // 
+            this.hideAnswer6.BackColor = System.Drawing.Color.Black;
+            this.hideAnswer6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.hideAnswer6.Location = new System.Drawing.Point(264, 320);
+            this.hideAnswer6.Name = "hideAnswer6";
+            this.hideAnswer6.Size = new System.Drawing.Size(100, 23);
+            this.hideAnswer6.TabIndex = 30;
+            this.hideAnswer6.UseVisualStyleBackColor = false;
+            this.hideAnswer6.Click += new System.EventHandler(this.hideAnswer_Click);
+            // 
+            // hideAnswer7
+            // 
+            this.hideAnswer7.BackColor = System.Drawing.Color.Black;
+            this.hideAnswer7.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.hideAnswer7.Location = new System.Drawing.Point(264, 370);
+            this.hideAnswer7.Name = "hideAnswer7";
+            this.hideAnswer7.Size = new System.Drawing.Size(100, 23);
+            this.hideAnswer7.TabIndex = 31;
+            this.hideAnswer7.UseVisualStyleBackColor = false;
+            this.hideAnswer7.Click += new System.EventHandler(this.hideAnswer_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(74, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(386, 20);
+            this.textBox1.TabIndex = 32;
+            this.textBox1.Text = "FinnishLanguage.txt";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 33;
+            this.label1.Text = "File:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(474, 529);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.hideAnswer7);
+            this.Controls.Add(this.hideAnswer6);
+            this.Controls.Add(this.hideAnswer5);
+            this.Controls.Add(this.hideAnswer4);
+            this.Controls.Add(this.hideAnswer3);
+            this.Controls.Add(this.hideAnswer2);
+            this.Controls.Add(this.hideAnswer1);
+            this.Controls.Add(this.Englbl7);
+            this.Controls.Add(this.Englbl6);
+            this.Controls.Add(this.Englbl5);
+            this.Controls.Add(this.Englbl4);
+            this.Controls.Add(this.Englbl3);
+            this.Controls.Add(this.Englbl2);
+            this.Controls.Add(this.Englbl1);
+            this.Controls.Add(this.refrshbtn);
+            this.Controls.Add(this.entry2);
+            this.Controls.Add(this.entry1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Vocablbl7);
+            this.Controls.Add(this.Vocablbl6);
+            this.Controls.Add(this.Vocablbl5);
+            this.Controls.Add(this.Vocablbl4);
+            this.Controls.Add(this.Vocablbl3);
+            this.Controls.Add(this.Vocablbl2);
+            this.Controls.Add(this.Vocablbl1);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Name = "Form1";
+            this.Text = "Vocabulary App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Vocablbl1;
+        private System.Windows.Forms.Label Vocablbl2;
+        private System.Windows.Forms.Label Vocablbl3;
+        private System.Windows.Forms.Label Vocablbl4;
+        private System.Windows.Forms.Label Vocablbl5;
+        private System.Windows.Forms.Label Vocablbl6;
+        private System.Windows.Forms.Label Vocablbl7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox entry1;
+        private System.Windows.Forms.TextBox entry2;
+        private System.Windows.Forms.Button refrshbtn;
+        private System.Windows.Forms.TextBox Englbl1;
+        private System.Windows.Forms.TextBox Englbl2;
+        private System.Windows.Forms.TextBox Englbl3;
+        private System.Windows.Forms.TextBox Englbl4;
+        private System.Windows.Forms.TextBox Englbl5;
+        private System.Windows.Forms.TextBox Englbl6;
+        private System.Windows.Forms.TextBox Englbl7;
+        private System.Windows.Forms.Button hideAnswer1;
+        private System.Windows.Forms.Button hideAnswer2;
+        private System.Windows.Forms.Button hideAnswer3;
+        private System.Windows.Forms.Button hideAnswer4;
+        private System.Windows.Forms.Button hideAnswer5;
+        private System.Windows.Forms.Button hideAnswer6;
+        private System.Windows.Forms.Button hideAnswer7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Vocabulary/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+
+namespace Vocabulary
+{
+    public partial class Form1 : Form
+    {
+        private static System.Timers.Timer reTimer;
+        int choice;
+        int timeelap = 5;
+
+        public Form1()
+        {
+            InitializeComponent();
+            generate_vocab();
+            this.CenterToScreen();
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+        }
+
+        //methods to generate vocabs
+        private void generate_vocab()
+        {
+            try
+            {
+                string[] voc = { };
+                //get path of document folder and read vocab file
+                string customFile = textBox1.Text;
+                string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                string file = $"{path}\\{customFile}";
+                string[] langfile = File.ReadAllLines(file);
+                int max = langfile.Length; //set max to amount of lines in array
+
+                //generate first set
+                generateRandom(max);
+                voc = langfile[choice].Split(':'); //split array at :
+                langfile = langfile.Except(new string[] { langfile[choice] }).ToArray(); //re-create array without [choice]
+                Vocablbl1.Text = voc[0];
+                Englbl1.Text = voc[1];
+
+                //generate second set
+                generateRandom(max);
+                voc = langfile[choice].Split(':'); //split array at :
+                langfile = langfile.Except(new string[] { langfile[choice] }).ToArray(); //re-create array without [choice]
+                Vocablbl2.Text = voc[0];
+                Englbl2.Text = voc[1];
+
+                //generate third set
+                generateRandom(max);
+                voc = langfile[choice].Split(':'); //split array at :
+                langfile = langfile.Except(new string[] { langfile[choice] }).ToArray(); //re-create array without [choice]
+                Vocablbl3.Text = voc[0];
+                Englbl3.Text = voc[1];
+
+                //generate fourth set
+                generateRandom(max);
+                voc = langfile[choice].Split(':'); //split array at :
+                langfile = langfile.Except(new string[] { langfile[choice] }).ToArray(); //re-create array without [choice]
+                Vocablbl4.Text = voc[0];
+                Englbl4.Text = voc[1];
+
+                //generate fifth set
+                generateRandom(max);
+                voc = langfile[choice].Split(':'); //split array at :
+                langfile = langfile.Except(new string[] { langfile[choice] }).ToArray(); //re-create array without [choice]
+                Vocablbl5.Text = voc[0];
+                Englbl5.Text = voc[1];
+
+                //generate sixth set
+                generateRandom(max);
+                voc = langfile[choice].Split(':'); //split array at :
+                langfile = langfile.Except(new string[] { langfile[choice] }).ToArray(); //re-create array without [choice]
+                Vocablbl6.Text = voc[0];
+                Englbl6.Text = voc[1];
+
+                //generate seventh set
+                generateRandom(max);
+                voc = langfile[choice].Split(':'); //split array at :
+                langfile = langfile.Except(new string[] { langfile[choice] }).ToArray(); //re-create array without [choice]
+                Vocablbl7.Text = voc[0];
+                Englbl7.Text = voc[1];
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(choice);
+            }
+        }
+
+        //random selection
+        private void generateRandom(int max)
+        {
+            Random rnd = new Random();
+            choice = rnd.Next(0, max);
+        }
+
+        //save entered vocabulary in file on button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //get path of document folder and read vocab file
+            string customFile = textBox1.Text;
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string file = $"{path}\\{customFile}";
+            //jigsaw together vocab for saving
+            String totalentry = $"{entry1.Text}:{entry2.Text}";
+            //read .txt file to check if already present, save if not
+            string[] langfile = File.ReadAllLines(file);
+            if (langfile.Contains(totalentry))
+            {
+                MessageBox.Show("This Vocabulary is already present.");
+            }
+            else
+            {
+                File.AppendAllText(file, string.Format("{0}{1}", $"{totalentry}", Environment.NewLine));
+            }
+        }
+
+        //refresh, disable btn and start cooldown on refresh click
+        private void refrshbtn_Click(object sender, EventArgs e)
+        {
+            generate_vocab();
+            refrshbtn.Enabled = false;
+            refrh_timer();
+            reTimer.Start();
+            hideAnswer1.Visible = true;
+            hideAnswer2.Visible = true;
+            hideAnswer3.Visible = true;
+            hideAnswer4.Visible = true;
+            hideAnswer5.Visible = true;
+            hideAnswer6.Visible = true;
+            hideAnswer7.Visible = true;
+        }
+
+        //create 1s timer
+        private void refrh_timer()
+        {
+            reTimer = new System.Timers.Timer(1000);
+            reTimer.Elapsed += OnTimedEvent;
+            reTimer.Enabled = true;
+        }
+
+        //change refresh button text on elapsed like a countdown
+        private void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            timeelap--;
+            switch (timeelap)
+            {
+                case 5:
+                    refrshbtn.Invoke(new MethodInvoker(() => refrshbtn.Text = "Refresh (5s)"));
+                    break;
+                case 4:
+                    refrshbtn.Invoke(new MethodInvoker(() => refrshbtn.Text = "Refresh (4s)"));
+                    break;
+                case 3:
+                    refrshbtn.Invoke(new MethodInvoker(() => refrshbtn.Text = "Refresh (3s)"));
+                    break;
+                case 2:
+                    refrshbtn.Invoke(new MethodInvoker(() => refrshbtn.Text = "Refresh (2s)"));
+                    break;
+                case 1:
+                    refrshbtn.Invoke(new MethodInvoker(() => refrshbtn.Text = "Refresh (1s)"));
+                    break;
+                case 0:
+                    reTimer.Dispose();
+                    refrshbtn.Invoke(new MethodInvoker(() => refrshbtn.Text = "Refresh"));
+                    refrshbtn.Invoke(new MethodInvoker(() => refrshbtn.Enabled=true));
+                    timeelap = 6;
+                    break;
+            }
+        }
+
+        private void onClick(Control c)
+        {
+            c.Visible = false;
+        }
+
+        private void hideAnswer_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            onClick(btn);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            generate_vocab();
+        }
+    }
+}
